a464395714fe136c8ac45ccd6bb18f8b493ce98f
lz-kwok <lz_kwok@163.com>
2019-11-18 22:52:20 +0800

    add version update

--- bsp/stm32/stm32f407-st-discovery/applications/main.c
+++ bsp/stm32/stm32f407-st-discovery/applications/main.c
@@ -20,6 +20,11 @@
 #define gprs_rst            GET_PIN(G, 1)
 #define usbd                GET_PIN(C, 9)
 
+#define MajorVer            1
+#define MiddleVer           0
+#define MinnorVer           0
+
+uint8_t sendBuf[10] = {0x0D,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x0D};
 
 void phy_reset(void)
 {
@@ -40,8 +45,6 @@ int main(void)
     rt_pin_write(usbd, PIN_HIGH);
 
     rt_device_t dev = RT_NULL;
-    char buf[] = "hello zhizhuo\r\n";
-
     dev = rt_device_find("vcom");
    
     if (dev)
@@ -53,10 +56,19 @@ int main(void)
     {   
         recvLen = rt_device_read(dev,0,dataRecv,32);
         if(recvLen > 0){
-            rt_device_write(dev, 0, &dataRecv, recvLen);
+            if((dataRecv[0] == 0x0D)&&(dataRecv[recvLen - 1] == 0x0D)){
+                switch(dataRecv[1])
+                {
+                case 0xFE:       //查询版本
+                    sendBuf[1] = dataRecv[1];
+                    sendBuf[2] = MajorVer;
+                    sendBuf[3] = MiddleVer;
+                    sendBuf[4] = MinnorVer;
+                    rt_device_write(dev, 0, sendBuf, 10);
+                break;
+                }
+            }
         }
-
-        rt_device_write(dev, 0, buf, strlen(buf));
         
         rt_thread_mdelay(10);
     }
