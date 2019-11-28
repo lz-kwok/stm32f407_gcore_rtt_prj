627560dc0e67e07f08cffdef443f20ff3e462f69
guoliangzhi <guoliangzhi@skyworth.com>
2019-11-28 16:09:23 +0800

    Optimize serial port function API

--- bsp/stm32/stm32f407-st-discovery/applications/g_uart.c
+++ bsp/stm32/stm32f407-st-discovery/applications/g_uart.c
@@ -17,9 +17,12 @@
 #define UART_RX_EVENT (1 << 0)
 
 /* 事件控制块 */
-static struct rt_event event;
+static struct rt_event g_event1;
 /* 串口设备句柄 */
-static rt_device_t uart_device = RT_NULL;
+static rt_device_t g_uart1 = RT_NULL;
+const char *uart1_name = "uart1";
+
+
 
 static struct serial_configure dpsp_useconfig = {
     BAUD_RATE_115200,
@@ -36,33 +39,33 @@ static struct serial_configure dpsp_useconfig = {
 static rt_err_t uart_intput(rt_device_t dev, rt_size_t size)
 {
     /* 发送事件 */
-    rt_event_send(&event, UART_RX_EVENT);
+    rt_event_send(&g_event1, UART_RX_EVENT);
     
     return RT_EOK;
 }
 
-rt_uint8_t uart_getchar(void)
+rt_uint8_t uart_getchar(rt_device_t dev)
 {
     rt_uint32_t e;
     rt_uint8_t ch;
     
     /* 读取1字节数据 */
-    while (rt_device_read(uart_device, 0, &ch, 1) != 1)
+    while (rt_device_read(dev, 0, &ch, 1) != 1)
     {
          /* 接收事件 */
-        rt_event_recv(&event, UART_RX_EVENT,RT_EVENT_FLAG_AND | RT_EVENT_FLAG_CLEAR,RT_WAITING_FOREVER, &e);
+        rt_event_recv(&g_event1, UART_RX_EVENT,RT_EVENT_FLAG_AND | RT_EVENT_FLAG_CLEAR,RT_WAITING_FOREVER, &e);
     }
 
     return ch;
 }
 
-void uart_putchar(const rt_uint8_t c)
+void uart_putchar(rt_device_t dev,const rt_uint8_t c)
 {
     rt_size_t len = 0;
     rt_uint32_t timeout = 0;
     do
     {
-        len = rt_device_write(uart_device, 0, &c, 1);
+        len = rt_device_write(dev, 0, &c, 1);
         timeout++;
     }
     while (len != 1 && timeout < 500);
@@ -70,58 +73,55 @@ void uart_putchar(const rt_uint8_t c)
 
 
 
-rt_err_t uart_open(const char *name)
+rt_device_t uart_open(const char *name)
 {
     rt_err_t res;
-                                 
+    rt_device_t dev;                             
     /* 查找系统中的串口设备 */
-    uart_device = rt_device_find(name);   
+    dev = rt_device_find(name);   
     /* 查找到设备后将其打开 */
-    if (uart_device != RT_NULL)
+    if (dev != RT_NULL)
     {   
        
-        res = rt_device_set_rx_indicate(uart_device, uart_intput);
+        res = rt_device_set_rx_indicate(dev, uart_intput);
         /* 检查返回值 */
         if (res != RT_EOK)
         {
             rt_kprintf("set %s rx indicate error.%d\n",name,res);
-            return -RT_ERROR;
+            return RT_NULL;
         }
 
         /* 打开设备，以可读写、中断方式 */
-        res = rt_device_open(uart_device, RT_DEVICE_OFLAG_RDWR | RT_DEVICE_FLAG_INT_RX );       
+        res = rt_device_open(dev, RT_DEVICE_OFLAG_RDWR | RT_DEVICE_FLAG_INT_RX );       
         /* 检查返回值 */
         if (res != RT_EOK)
         {
             rt_kprintf("open %s device error.%d\n",name,res);
-            return -RT_ERROR;
+            return RT_NULL;
         }
 
-        res = rt_device_control(uart_device,RT_DEVICE_CTRL_CONFIG,(void *)&dpsp_useconfig);
+        res = rt_device_control(dev,RT_DEVICE_CTRL_CONFIG,(void *)&dpsp_useconfig);
         if (res != RT_EOK)
         {
             rt_kprintf("control %s device error.%d\n",name,res);
-            return -RT_ERROR;
+            return RT_NULL;
         }
     }
     else
     {
         rt_kprintf("can't find %s device.\n",name);
-        return -RT_ERROR;
+        return RT_NULL;
     }
-
-    /* 初始化事件对象 */
-    rt_event_init(&event, "event", RT_IPC_FLAG_FIFO); 
             
-    return RT_EOK;
+    return dev;
 }
 
 
-void g_uart_sendto_Dpsp(const rt_uint8_t *cmd)
+void g_uart_sendto_Dpsp(rt_device_t dev,const rt_uint8_t *cmd)
 {
     while(*cmd)
     {
-        uart_putchar(*cmd++);
+        uart_putchar(dev,*cmd++);
     }
 }
 
@@ -132,14 +132,16 @@ void uart_thread_entry(void* parameter)
     rt_uint8_t uart_recv_num = 0;
     
     /* 打开串口 */
-    if (uart_open("uart1") != RT_EOK)
-    {
-        rt_kprintf("uart open error.\n");
-         while (1)
-         {
+    g_uart1 = uart_open(uart1_name);
+    if(g_uart1 == RT_NULL){
+        rt_kprintf("%s open error.\n",uart1_name);
+        while (1)
+        {
             rt_thread_delay(10);
-         }
+        }
     }
+    /* 初始化事件对象 */
+    rt_event_init(&g_event1, "g_event1", RT_IPC_FLAG_FIFO); 
        
     rt_kprintf("%s\n",__func__);
     
