9a73872679e408869a72d4f2293960edfd80507e
guoliangzhi <guoliangzhi@skyworth.com>
2019-11-29 15:57:51 +0800

    read adc val

--- bsp/stm32/stm32f407-st-discovery/applications/main.c
+++ bsp/stm32/stm32f407-st-discovery/applications/main.c
@@ -22,7 +22,11 @@
 #define gprs_rst            GET_PIN(G, 1)
 #define usbd                GET_PIN(C, 9)
 
+#define ADC_DEV_NAME        "adc3"  
 
+
+rt_adc_device_t adc_dev; 
+rt_uint32_t adc_val[10];
 
 
 void phy_reset(void)
@@ -44,11 +48,18 @@ int main(void)
     g_usb_cdc_init();
     g_uart_init();
     rt_thread_mdelay(1000);
+
+    adc_dev = (rt_adc_device_t)rt_device_find(ADC_DEV_NAME);
+    rt_adc_enable(adc_dev, 4);
     
     while (RT_TRUE)
     {   
-        
-        rt_thread_mdelay(5000);
+        adc_val[0] = rt_adc_read(adc_dev, 5);
+        adc_val[1] = rt_adc_read(adc_dev, 6);
+        adc_val[2] = rt_adc_read(adc_dev, 7);
+        adc_val[3] = rt_adc_read(adc_dev, 8);
+
+        rt_thread_mdelay(1000);
     }
 
     return RT_EOK;
