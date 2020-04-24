1409c4bb6bd8eaa0866dd464a6f4a23c4783a98d
guoliangzhi <lz_kwok@163.com>
2020-04-24 16:15:26 +0800

    spi mode tf card interface

--- components/rl-arm/RL-FlashFS/src/FS_MicroSD_Cardprg.c
+++ components/rl-arm/RL-FlashFS/src/FS_MicroSD_Cardprg.c
@@ -1,14 +1,14 @@
 /*
 *********************************************************************************************************
 *
-*	Ä£¿éÃû³Æ : FlashFSµÄ½Ó¿ÚÎÄ¼þ
-*	ÎÄ¼þÃû³Æ : FS_MicroSD_Cardprg.c
-*	°æ    ±¾ : V1.0
-*	Ëµ    Ã÷ : FlashFS¶ÔMicro SD¿¨µÄ½Ó¿ÚÎÄ¼þ
+*	æ¨¡å—åç§° : FlashFSçš„æŽ¥å£æ–‡ä»¶
+*	æ–‡ä»¶åç§° : FS_MicroSD_Cardprg.c
+*	ç‰ˆ    æœ¬ : V1.0
+*	è¯´    æ˜Ž : FlashFSå¯¹Micro SDå¡çš„æŽ¥å£æ–‡ä»¶
 *
-*	ÐÞ¸Ä¼ÇÂ¼ :
-*		°æ±¾ºÅ  ÈÕÆÚ        ×÷Õß       ËµÃ÷
-*		V1.0    2020-04-24  leonGuo  ²âÊÔ°æ±¾
+*	ä¿®æ”¹è®°å½• :
+*		ç‰ˆæœ¬å·  æ—¥æœŸ        ä½œè€…       è¯´æ˜Ž
+*		V1.0    2020-03-24  leonGuo  æµ‹è¯•ç‰ˆæœ¬
 *
 *	Copyright (C), 2020-present, lz_kwok@163.com
 *
@@ -21,116 +21,78 @@
 /*-----------------------------------------------------------------------------
  *      NAND driver prototypes
  *----------------------------------------------------------------------------*/
-static U32 Init         (NAND_DRV_CFG *cfg);
-static U32 UnInit       (NAND_DRV_CFG *cfg);
-static U32 PageRead     (U32 row, U8 *buf, NAND_DRV_CFG *cfg);
-static U32 PageWrite    (U32 row, U8 *buf, NAND_DRV_CFG *cfg);
-static U32 BlockErase   (U32 row, NAND_DRV_CFG *cfg);
-
+static BOOL Init         (void);
+static BOOL UnInit       (void);
+static U8 Send           (U8 outb);
+static BOOL SendBuf      (U8 *buf, U32 sz);
+static BOOL RecBuf       (U8 *buf, U32 sz);
+static BOOL BusSpeed     (U32 kbaud);
+static BOOL SetSS        (U32 ss);
+static void CheckMedia   (void);
 /*-----------------------------------------------------------------------------
   NAND Device Driver Control Block
  *----------------------------------------------------------------------------*/
-const NAND_DRV spi0_drv = {
+const SPI_DRV spi0_drv = {
   Init,
   UnInit,
-  PageRead,
-  PageWrite,
-  BlockErase,
+  Send,
+  SendBuf,
+  RecBuf,
+  BusSpeed,
+  SetSS,
+  CheckMedia
 };
 
 
-/*-----------------------------------------------------------------------------
- *      Initialise NAND flash driver
- *  *cfg = Pointer to configuration structure
- *
- *  Return: RTV_NOERR         - NAND Flash Initialisation successful
- *          ERR_NAND_HW_TOUT  - NAND Flash Reset Command failed
- *          ERR_INVALID_PARAM - Invalid parameter
- *----------------------------------------------------------------------------*/
-static U32 Init (NAND_DRV_CFG *cfg) {
-	
-  /* Define spare area layout */
-  cfg->PgLay->Pos_LSN = 0;
-  cfg->PgLay->Pos_COR = 4;
-  cfg->PgLay->Pos_BBM = 5;
-  cfg->PgLay->Pos_ECC = 6;
 
-  /* Define page organization */
-  cfg->PgLay->SectInc  =  512;
-  cfg->PgLay->SpareOfs =  2048;
-  cfg->PgLay->SpareInc =  16;
+static BOOL Init (void) {
 	
-  rt_hw_mtd_nand_init();      //NAND_Init();
 
-  return RTV_NOERR;
+	
+  return __TRUE;
 }
 
-/*-----------------------------------------------------------------------------
- *      Uninitialise NAND flash driver
- *  *cfg = Pointer to configuration structure
- *
- *  Return: RTV_NOERR         - UnInit successful
- *----------------------------------------------------------------------------*/
-static U32 UnInit (NAND_DRV_CFG *cfg) {
-   rt_hw_mtd_nand_deinit();      //NAND_UnInit();
-   return RTV_NOERR;
+
+static BOOL UnInit (void) {
+
+
+   return __TRUE;
 }
 
-/*-----------------------------------------------------------------------------
- *      Read page
- *  row  = Page address
- *  *buf = Pointer to data buffer
- *  *cfg = Pointer to configuration structure
- *
- *  Return: RTV_NOERR         - Page read successful
- *          ERR_NAND_HW_TOUT  - Hardware transfer timeout
- *          ERR_ECC_COR       - ECC corrected the data within page
- *          ERR_ECC_UNCOR     - ECC was not able to correct the data
- *----------------------------------------------------------------------------*/
-static U32 PageRead (U32 row, U8 *buf, NAND_DRV_CFG *cfg) {
+
+static U8 Send(U8 outb){
    U8 Status;
 	
-   Status = FSMC_NAND_ReadPage(buf, row, 0, NAND_PAGE_TOTAL_SIZE);
-   // Status = NAND_ReadPage(row,0,buf,NAND_PAGE_TOTAL_SIZE);
+
    return Status;
 }
 
-/*-----------------------------------------------------------------------------
- *      Write page
- *  row  = Page address
- *  *buf = Pointer to data buffer
- *  *cfg = Pointer to configuration structure
- *
- *  Return: RTV_NOERR         - Page write successful
- *          ERR_NAND_PROG     - Page write failed
- *          ERR_NAND_HW_TOUT  - Hardware transfer timeout
- *----------------------------------------------------------------------------*/
-static U32 PageWrite (U32 row, U8 *buf, NAND_DRV_CFG *cfg) {
+
+static BOOL SendBuf(U8 *buf, U32 sz)
+{
    U8 Status;
 
-   Status = FSMC_NAND_WritePage(buf, row, 0, NAND_PAGE_TOTAL_SIZE);
-   // Status = NAND_WritePage(row,0,buf,NAND_PAGE_TOTAL_SIZE);
    return Status;
 }
 
-/*-----------------------------------------------------------------------------
- *      Erase block
- *  row  = Block address
- *  *cfg = Pointer to configuration structure
- *
- *  Return: RTV_NOERR         - Block erase successful
- *          ERR_NAND_ERASE    - Block erase failed
- *          ERR_NAND_HW_TOUT  - Hardware transfer timeout
- *----------------------------------------------------------------------------*/
-static U32 BlockErase (U32 row, NAND_DRV_CFG *cfg) {
-   U32 ulPBN;
+static BOOL RecBuf(U8 *buf, U32 sz)
+{
    U8 Status;
-	
-   ulPBN = row / cfg->NumPages;
-   Status = NAND_EraseBlock(ulPBN);
+
    return Status;
 }
 
-/*-----------------------------------------------------------------------------
- * end of file
- *----------------------------------------------------------------------------*/
+static BOOL BusSpeed(U32 kbaud)
+{
+   return __TRUE;
+}
+
+static BOOL SetSS(U32 ss)
+{
+   return __TRUE;
+}
+
+static void CheckMedia(void)
+{
+   
+}
